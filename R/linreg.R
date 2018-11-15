906ab403a01dc8ae6989808d6c95c3c94d8bb1e4
wu292 <ywu@hsph.harvard.edu>
2018-11-15 12:46:24 -0500

    first try!

--- R/linreg.R
+++ R/linreg.R
@@ -1,3 +1,20 @@
+#' Linear regression
+#'
+#' Runs an OLS regression not unlike \code{\link{lm}}
+#'
+#' @param y response vector (1 x n)
+#' @param X covariate matrix (p x n) with no intercept
+#'
+#' @return A list with 4 elements: coefficients, vcov, sigma, df
+#'
+#' @examples
+#' data(mtcars)
+#' X <- as.matrix(mtcars[, c("cyl", "disp", "hp")])
+#' y <- mtcars[, "mpg"]
+#' linmodEst(y, X)
+#'
+#' @export
+#'
 linmodEst <- function(x, y) {
   coef <- solve(t(x) %*% x) %*% t(x) %*% y
   ## degrees of freedom and standard deviation of residuals
